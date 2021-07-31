800c659cc7155124a6fa5f11ecef4f21158cdac5
Carlos Gabriel <carlosgabrielpc@live.com>
2021-07-31 18:05:41 -0300

    Cartas girando com animaçao

--- scripts.js
+++ scripts.js
@@ -1,3 +1,44 @@
-function desenharCartas(quantidade) {
+let ultimoCardVirado;
+let cartas = [];
+let cartasViradas = []; 
+
+function checarNumero() {
+    while(true){
+        const qc = prompt("Escolha a quantidade de cartas (numero par entre 4 e 14): ");
+        if (qc != undefined && qc > 3 && qc < 15 && qc % 2 == 0){
+            embaralharCartas(Number(qc));
+            break;
+        }
+    }
+}
+
+function embaralharCartas(quantidadeCartas) {
+    let possivelEmb = ["bobross","explody","fiesta","metal","revertit","triplets", "unicorn"].sort(alet);
+
+    for (let i = 0; i < (quantidadeCartas/2); i++) cartas.push(possivelEmb[i], possivelEmb[i]);
     
+    desenharCartas(cartas.sort(alet));
+}
+
+function alet() {
+    return Math.random() - 0.5;
+}
+
+function desenharCartas(cartas) {
+    cartas.forEach(e => {
+        document.querySelector("main").innerHTML += 
+        `<div class="card" id=${e} onclick="virarCard(this)">
+            <div class="front-face face flex-cc">
+                <img src="assets/front.png" alt="Parrot">
+            </div>
+            <div class="back-face face flex-cc">
+                <img src="assets/${e}parrot.gif">
+            </div>
+        </div>`;});    
+}
+
+function virarCard(card) {
+
+    card.firstElementChild.classList.toggle("abrir");
+    card.lastElementChild.classList.toggle("fechar");
 }
